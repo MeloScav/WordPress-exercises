0f75b81456a73d4a7ebbdef4713a506bb853db43
Méloé <meloscavezzoni@gmail.com>
2021-05-05 15:00:32 +0200

    cards structures with articles

--- wp-content/themes/my-theme/index.php
+++ wp-content/themes/my-theme/index.php
@@ -9,17 +9,24 @@
 // Check if we have articles
 if (have_posts()) :
 ?>
-    <ul>
+
+    <div class="row">
         <?php while (have_posts()) : the_post(); ?>
-            <li>
-                <a href="<?php the_permalink(); ?>">
-                    <?php the_title(); ?>
-                </a>
-                -
-                <?php the_author(); ?>
-            </li>
+            <div class="col-sm-4">
+                <div class="card" style="width: 18rem;">
+                    <?php the_post_thumbnail('medium', ['class' => 'card-img-top', 'alt' => '', 'style' => 'height: auto']) ?>
+                    <div class="card-body">
+                        <h5 class="card-title"><?php the_title() ?></h5>
+                        <h6 class="card-subtitle mb-2 text-muted"><?php the_category() ?></h6>
+                        <p class="card-text">
+                            <?php the_excerpt() ?>
+                        </p>
+                        <a href="<?php the_permalink() ?>" class="card-link">Voir plus</a>
+                    </div>
+                </div>
+            </div>
         <?php endwhile ?>
-    </ul>
+    </div>
 
 <?php else : ?>
     <h2>Pas d'articles</h2>
