56fe0b80d88b02bd47ebebbe68d4424e5e0b7838
Méloé <meloscavezzoni@gmail.com>
2021-05-05 12:13:37 +0200

    Articles added

--- wp-content/themes/my-theme/index.php
+++ wp-content/themes/my-theme/index.php
@@ -1,8 +1,29 @@
 <?php get_header(); ?>
-Bonjour tout le monde
+<h1>Bonjour tout le monde</h1>
 <?php
 // wp_title();
 ?>
 
+<!-- Display articles -->
+<?php
+// Check if we have articles
+if (have_posts()) :
+?>
+    <ul>
+        <?php while (have_posts()) : the_post(); ?>
+            <li>
+                <a href="<?php the_permalink(); ?>">
+                    <?php the_title(); ?>
+                </a>
+                -
+                <?php the_author(); ?>
+            </li>
+        <?php endwhile ?>
+    </ul>
+
+<?php else : ?>
+    <h2>Pas d'articles</h2>
+
+<?php endif; ?>
 
 <?php get_footer(); ?>
