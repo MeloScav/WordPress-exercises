79c2c7c3928fe4c0d6d1f08b5d796e36170a8d62
Méloé <meloscavezzoni@gmail.com>
2021-05-05 14:59:47 +0200

    Jquery link correction + post-thumbnails added

--- wp-content/themes/my-theme/functions.php
+++ wp-content/themes/my-theme/functions.php
@@ -3,6 +3,7 @@
 function mytheme_supports()
 {
     add_theme_support('title-tag');
+    add_theme_support('post-thumbnails');
 }
 
 function mytheme_register_assets()
@@ -11,7 +12,7 @@ function mytheme_register_assets()
     wp_register_script('bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js', ['popper', 'jquery'], false, true);
     wp_register_script('popper', 'https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js', [], false, true);
     wp_deregister_script('jquery'); // To replace wp jquery with our own
-    wp_register_script('jquery', 'https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js', [], false, true);
+    wp_register_script('jquery', 'https://code.jquery.com/jquery-3.5.1.slim.min.js', [], false, true);
     wp_enqueue_style('bootstrap');
     wp_enqueue_script('bootstrap');
 }
