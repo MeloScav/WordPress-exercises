0ea1205cc2b581a92b616373bd67e99afe30876f
Méloé <meloscavezzoni@gmail.com>
2021-05-06 15:04:04 +0200

    Basic content added

---
+++ wp-content/themes/my-theme/single-post.php
@@ -0,0 +1,14 @@
+<?php get_header(); ?>
+<!-- Display articles -->
+<?php
+// Check if we have articles
+if (have_posts()) :
+    while (have_posts()) : the_post(); ?>
+        <h1><?php the_title() ?></h1>
+        <img src="<?php the_post_thumbnail_url() ?>" alt="" style="width: 100%; height:auto;">
+
+        <?php the_content() ?>
+<?php endwhile;
+endif; ?>
+
+<?php get_footer(); ?>
