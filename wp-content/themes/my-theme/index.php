7157a56b8989027f1aa0f62ad810520bf8586618
Méloé <meloscavezzoni@gmail.com>
2021-05-11 13:07:20 +0200

    Post thumbnail + pagging

--- wp-content/themes/my-theme/index.php
+++ wp-content/themes/my-theme/index.php
@@ -14,7 +14,7 @@ if (have_posts()) :
         <?php while (have_posts()) : the_post(); ?>
             <div class="col-sm-4">
                 <div class="card" style="width: 18rem;">
-                    <?php the_post_thumbnail('medium', ['class' => 'card-img-top', 'alt' => '', 'style' => 'height: auto']) ?>
+                    <?php the_post_thumbnail('post-thumbnail', ['class' => 'card-img-top', 'alt' => '', 'style' => 'height: auto']) ?>
                     <div class="card-body">
                         <h5 class="card-title"><?php the_title() ?></h5>
                         <h6 class="card-subtitle mb-2 text-muted"><?php the_category() ?></h6>
@@ -28,8 +28,17 @@ if (have_posts()) :
         <?php endwhile; ?>
     </div>
 
+    <?php mytheme_pagination(); ?>
+
+    <?php //the_posts_pagination(); 
+    ?>
+    <?php // echo next_posts_link(); 
+    ?>
+    <?php // echo previous_posts_link(); 
+    ?>
+
 <?php else : ?>
-    <h2>Pas d'articles</h2>
+    <h1>Pas d'articles</h1>
 
 <?php endif; ?>
 
