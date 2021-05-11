0039aac2d3fbe8498f2a791996516b8b8a332849
Méloé <meloscavezzoni@gmail.com>
2021-05-11 13:05:51 +0200

    Image size + paging + sponso meta box

--- wp-content/themes/my-theme/functions.php
+++ wp-content/themes/my-theme/functions.php
@@ -7,6 +7,11 @@ function mytheme_supports()
     add_theme_support('menus');
     register_nav_menu('header', 'En tête du menu');
     register_nav_menu('footer', 'Pied de page');
+
+    add_image_size('post-thumbnail', 350, 215, true);
+    // add_image_size('card-header', 350, 215, true);
+    // remove_image_size('medium');
+    // add_image_size('medium', 500, 500);
 }
 
 function mytheme_register_assets()
@@ -52,6 +57,29 @@ function mytheme_menu_link_class($attrs)
     return $attrs;
 }
 
+function mytheme_pagination()
+{
+    $pages = paginate_links(['type' => 'array']);
+    if ($pages === null) {
+        return;
+    }
+
+    echo '<nav aria-label="Page navigation example" class="my-4">';
+    echo '<ul class="pagination">';
+
+    foreach ($pages as $page) {
+        $active = strpos($page, 'current') !== false;
+        $class = 'page-item';
+        if ($active) {
+            $class .= ' active';
+        }
+        echo '<li class="' . $class . '">';
+        echo str_replace('page-numbers', 'page-link', $page);
+        echo '</li>';
+    }
+    echo '</ul';
+    echo '</nav>';
+}
 
 add_action('after_setup_theme', 'mytheme_supports');
 add_action('wp_enqueue_scripts', 'mytheme_register_assets');
@@ -60,3 +88,6 @@ add_filter('document_title_separator', 'mytheme_title_separator');
 // add_filter('document_title_parts', 'mytheme_document_title_parts');
 add_filter('nav_menu_css_class', 'mytheme_menu_class');
 add_filter('nav_menu_link_attributes', 'mytheme_menu_link_class');
+
+require_once('metaboxes/sponso.php');
+SponsoMetaBox::register();
