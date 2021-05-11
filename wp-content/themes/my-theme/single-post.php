cec4ffe54beee64563d1a4099e62ee62a82c10cc
Méloé <meloscavezzoni@gmail.com>
2021-05-11 13:08:51 +0200

    Content for the sponsored article added

--- wp-content/themes/my-theme/single-post.php
+++ wp-content/themes/my-theme/single-post.php
@@ -5,6 +5,13 @@
 if (have_posts()) :
     while (have_posts()) : the_post(); ?>
         <h1><?php the_title() ?></h1>
+
+        <?php if (get_post_meta(get_the_ID(), SponsoMetaBox::META_KEY, true) === '1') : ?>
+            <div class="alert alert-info">
+                Cet article est sponsorisé.
+            </div>
+        <?php endif ?>
+
         <img src="<?php the_post_thumbnail_url() ?>" alt="" style="width: 100%; height:auto;">
 
         <?php the_content() ?>
