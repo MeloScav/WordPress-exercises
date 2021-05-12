dae35ac3a7b1ed62417ea1b9038d6d9f87b9e42b
Méloé <meloscavezzoni@gmail.com>
2021-05-12 10:16:41 +0200

    Check permissions

--- wp-content/themes/my-theme/metaboxes/sponso.php
+++ wp-content/themes/my-theme/metaboxes/sponso.php
@@ -3,31 +3,40 @@
 class SponsoMetaBox
 {
     const META_KEY = 'mytheme_sponso';
+    const NONCE = '_mytheme_sponso_nonce';
 
     public static function register()
     {
-        add_action('add_meta_boxes', [self::class, 'add']);
+        add_action('add_meta_boxes', [self::class, 'add'], 10, 2);
         add_action('save_post', [self::class, 'save']);
     }
 
-    public static function add()
+    public static function add($postType, $post)
     {
-        add_meta_box(self::META_KEY, 'Sponsoring', [self::class, 'render'], 'post', 'side');
+        if ($postType === 'post' && current_user_can('publish_posts', $post)) {
+            add_meta_box(self::META_KEY, 'Sponsoring', [self::class, 'render'], 'post', 'side');
+        }
     }
 
     public static function render($post)
     {
         $value = get_post_meta($post->ID, self::META_KEY, true);
+        wp_nonce_field(self::NONCE, self::NONCE);
 ?>
-        <input type="hidden" value="0" name="<?php self::META_KEY ?>">
-        <input type="checkbox" value="1" name="<?php self::META_KEY ?>" <?php checked($value, '1') ?>>
+        <input type="hidden" value="0" name="<?= self::META_KEY ?>">
+        <input type="checkbox" value="1" name="<?= self::META_KEY ?>" <?php checked($value, '1') ?>>
         <label for="mythemesponso">Cet article est sponsorisé ?</label>
 <?php
     }
 
     public static function save($post)
     {
-        if (array_key_exists(self::META_KEY, $_POST) && current_user_can('edit_post', $post)) {
+
+        if (
+            array_key_exists(self::META_KEY, $_POST) &&
+            current_user_can('publish_posts', $post) &&
+            wp_verify_nonce($_POST[self::NONCE], self::NONCE)
+        ) {
             if ($_POST[self::META_KEY] === "0") {
                 delete_post_meta($post, self::META_KEY);
             } else {
