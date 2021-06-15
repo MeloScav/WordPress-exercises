6b86cae3da150f7e37702d2142879afaabb95bac
Méloé <meloscavezzoni@gmail.com>
2021-06-15 14:37:59 +0200

    Related articles added

--- wp-content/themes/my-theme/single-post.php
+++ wp-content/themes/my-theme/single-post.php
@@ -15,6 +15,43 @@ if (have_posts()) :
         <img src="<?php the_post_thumbnail_url() ?>" alt="" style="width: 100%; height:auto;">
 
         <?php the_content() ?>
+
+        <h2>Articles relatifs</h2>
+
+        <div class="row">
+            <?php
+            $sports = array_map(function ($term) {
+                return $term->term_id;
+            }, get_the_terms(get_post(), 'sport'));
+
+            $query = new WP_Query([
+                'post_not_in' => [get_the_ID()],
+                'post_type' => 'post',
+                'posts_per_page' => 3,
+                'orderby' => 'rand',
+                'tax_query' => [
+                    [
+                        'taxonomy' => 'sport',
+                        'terms' => $sports
+                    ]
+                ],
+                'meta_query' => [
+                    [
+                        'key' => SponsoMetaBox::META_KEY,
+                        'compare' => 'EXISTS'
+                    ]
+                ]
+            ]);
+
+            while ($query->have_posts()) : $query->the_post();
+            ?>
+                <div class="col-sm-4">
+                    <?php get_template_part('parts/card', 'post') ?>
+                </div>
+            <?php endwhile;
+            wp_reset_postdata(); ?>
+        </div>
+
 <?php endwhile;
 endif; ?>
 
